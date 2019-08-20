422932d56c3079e628deff4cbd4dee29f3abefdd
Andrew MacDonald <ammacdonald3@gmail.com>
2019-08-19 20:28:26 -0400

    Added index, all recipes, and layout HTML pages

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
-@app.route('/<name>')
+@app.route('/<string:name>')
 def hello_name(name):
-    return "Hello {}! Welcome to the site".format(name)
+    name = name.capitalize()
+    return f"Hello, {name}! Welcome to the site."
+
+@app.route('/all')
+def all():
+    recipes = ['Steak','Chicken','Veal']
+    return render_template('all.html', recipes=recipes)
+
 
 if __name__ == '__main__':
     app.run()
