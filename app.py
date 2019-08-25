0bdc513f30a9add798df2cb812c045fff9f093cf
Andrew MacDonald <ammacdonald3@gmail.com>
2019-08-25 18:56:48 -0400

    Removed requests module as no longer needed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
-import requests
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
