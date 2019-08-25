378fd85fbd6f8a0b6944924652a7d405e7be412d
Andrew MacDonald <ammacdonald3@gmail.com>
2019-08-25 18:49:55 -0400

    Established database connection POC

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
+import requests
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URL'] = ['DATABASE_URL']
 db = SQLAlchemy(app)
 
@@ -14,17 +15,32 @@ from models import Result
 def hello():
     return render_template('index.html')
 
-@app.route('/<string:name>')
-def hello_name(name):
-    name = name.capitalize()
-    return f"Hello, {name}! Welcome to the site."
+
+
+@app.route('/add', methods=['GET','POST'])
+def add_recipe():
+    output = []
+    results = {}
+    if request.method == "POST":
+        try:
+            result = Result(
+                recipe_name=request.form['recipe_name'],
+                recipe_desc=request.form['recipe_desc'],
+                recipe_prep_time=request.form['recipe_prep_time'],
+                recipe_cook_time=request.form['recipe_cook_time']
+            )
+            db.session.add(result)
+            db.session.commit()
+            output.append("Item successfully added!")
+        except:
+            output.append("Item did not add to database :(")
+    return render_template('add.html', output=output)
 
 @app.route('/all')
 def all():
     recipes = ['Steak','Chicken','Veal']
     return render_template('all.html', recipes=recipes)
 
-print(os.environ['APP_SETTINGS'])
 
 if __name__ == '__main__':
     app.run()
