013072d4871c2123e00cd3ae3545cbcbff3f1488
Andrew MacDonald <ammacdonald3@gmail.com>
2020-09-27 11:48:23 -0400

    Updated yields logic

--- app.py
+++ app.py
@@ -882,6 +882,10 @@ def auto_import():
 
             try:
                 clean_yields = re.sub('[^0-9]','', yields)
+                if clean_yields = None:
+                    clean_yields = 0
+                elif clean_yields = '':
+                    clean_yields = 0
             except:
                 clean_yields = 0
 
