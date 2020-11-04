b884ce7f9fc2d0de0e94589914a7986974701984
Andrew MacDonald <ammacdonald3@gmail.com>
2020-11-03 22:36:44 -0500

    Modal updates

--- static/all_recipes.js
+++ static/all_recipes.js
@@ -55,23 +55,35 @@ $(document).ready(function(){
                 $(mealId).attr("data-target", "#mealtime-modal-other-".concat(baseMealId))
             };
 
-            // If recipe is already on the user's meal plan, prompt whether to remove it
         } else {
+            // If recipe is already on the user's meal plan, prompt whether to remove it
             // User selects 'cancel' button (i.e. do not remove recipe from meal plan)
+            // BELOW CODE IS COMMENTED OUT BECAUSE MODAL HIDE IS HANDLED IN HTML
             /* $('.meal-button-cancel').click(function () {
                 $('.modal').modal('hide');
                 return;
             }) */
+
             // User selects 'proceed' button (i.e. remove recipe from meal plan)
             $('.meal-button-proceed').click(function () {
+
+                // Update color of Meal Plan button
                 $(mealId).addClass("btn-success")
                 $(mealId).removeClass("btn-danger")
+
+                // Change text of Meal Plan button
                 $(mealId).text("Add to Meal Plan")
+
+                // Change button data target so that it renders the Breakfast/Lunch/Dinner modal if clicked again
+                $(mealId).attr("data-target", "#mealtime-modal-other-".concat(baseMealId))
+
+                // Execute AJAX to remove from database
                 $.ajax({
                     url: '/_meal_plan',
                     type: 'POST',
                     data: { recipe_id:$(this).attr("name"), status:"unchecked" }
                 });
+
             })
         }
     });
@@ -80,67 +92,97 @@ $(document).ready(function(){
     // AJAX for adding recipe to user's meal plan as Breakfast
     $('.meal-breakfast').click(function () {
         var $this = $(this);
+        var baseMealId = $(this).attr("name")
         var mealId = ".meal-".concat($(this).attr("name"));
-        console.log(mealId)
-        $(mealId).addClass("btn-danger")
+        
+        // Update color of Meal Plan button
         $(mealId).removeClass("btn-success")
-        $(mealId).removeAttr("data-toggle")
-        $(mealId).removeAttr("data-target")
+        $(mealId).addClass("btn-danger")
+
+        // Change button data target so that it renders the Delete Confirm modal if clicked again
+        $(mealId).attr("data-target", "#mealtime-modal-other-confirm-delete-".concat(baseMealId))
+
+        // Change text of Meal Plan button
         $(mealId).text("Remove from Meal Plan")
+
+        // Hide the open modal (which allows user to select Breakfast/Lunch/Dinner)
         $('.modal').modal('hide');
+
+        // Execute AJAX to add to database
         $.ajax({
             url: '/_meal_breakfast',
             type: 'POST',
             data: { recipe_id:$(this).attr("name"), status:"checked" }
         });
-        bootbox.alert({
-            message: "Added to your breakfast meal plan!",
-            backdrop: true
-        });
+
+        // Render the "success" modal displaying that the recipe was added to the breakfast meal plan
+        var successModalId = "#mealtime-modal-other-success-".concat($(this).attr("name"));
+        $(successModalId).modal('show')
+
     });
 
     // AJAX for adding recipe to user's meal plan as Lunch
     $('.meal-lunch').click(function () {
         var $this = $(this);
+        var baseMealId = $(this).attr("name")
         var mealId = ".meal-".concat($(this).attr("name"));
-        console.log(mealId)
-        $(mealId).addClass("btn-danger")
+        
+        // Update color of Meal Plan button
         $(mealId).removeClass("btn-success")
-        $(mealId).removeAttr("data-toggle")
-        $(mealId).removeAttr("data-target")
+        $(mealId).addClass("btn-danger")
+
+        // Change button data target so that it renders the Delete Confirm modal if clicked again
+        $(mealId).attr("data-target", "#mealtime-modal-other-confirm-delete-".concat(baseMealId))
+
+        // Change text of Meal Plan button
         $(mealId).text("Remove from Meal Plan")
+
+        // Hide the open modal (which allows user to select Breakfast/Lunch/Dinner)
         $('.modal').modal('hide');
+
+        // Execute AJAX to add to database
         $.ajax({
-            url: '/_meal_lunch',
+            url: '/meal_lunch',
             type: 'POST',
             data: { recipe_id:$(this).attr("name"), status:"checked" }
         });
-        bootbox.alert({
-            message: "Added to your lunch meal plan!",
-            backdrop: true
-        });
+
+        // Render the "success" modal displaying that the recipe was added to the lunch meal plan
+        var successModalId = "#mealtime-modal-other-success-".concat($(this).attr("name"));
+        $(successModalId).modal('show')
+
     });
 
     // AJAX for adding recipe to user's meal plan as Dinner
     $('.meal-dinner').click(function () {
         var $this = $(this);
+        var baseMealId = $(this).attr("name")
         var mealId = ".meal-".concat($(this).attr("name"));
-        console.log(mealId)
-        $(mealId).addClass("btn-danger")
+        
+        // Update color of Meal Plan button
         $(mealId).removeClass("btn-success")
-        $(mealId).removeAttr("data-toggle")
-        $(mealId).removeAttr("data-target")
+        $(mealId).addClass("btn-danger")
+
+        // Change button data target so that it renders the Delete Confirm modal if clicked again
+        $(mealId).attr("data-target", "#mealtime-modal-other-confirm-delete-".concat(baseMealId))
+
+        // Change text of Meal Plan button
         $(mealId).text("Remove from Meal Plan")
+
+        // Hide the open modal (which allows user to select Breakfast/Lunch/Dinner)
         $('.modal').modal('hide');
+
+        // Execute AJAX to add to database
         $.ajax({
             url: '/_meal_dinner',
             type: 'POST',
             data: { recipe_id:$(this).attr("name"), status:"checked" }
         });
-        
+
+        // Render the "success" modal displaying that the recipe was added to the dinner meal plan
         var successModalId = "#mealtime-modal-other-success-".concat($(this).attr("name"));
-        console.log(successModalId)
         $(successModalId).modal('show')
+
     });
 });
 
