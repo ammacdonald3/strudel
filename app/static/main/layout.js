a9cfed90991af707a90dc5d9527b99c1860d9f64
Andrew MacDonald <ammacdonald3@gmail.com>
2022-05-09 16:35:11 -0400

    Fixes DOM loading timing for certain objects

--- app/static/main/layout.js
+++ app/static/main/layout.js
@@ -2,19 +2,35 @@ function navbarTogglePin() {
     // For mobile screen size, keep navbar toggle pinned to bottom and extend menu up
     if($(window).width() <= 500) {
         $('#navbarSupportedContent').addClass('order-1');
-        $('#navbar-brand').addClass('order-2');
-        $('#navbar-toggler').addClass('order-3');
+        $('#navbar-icon').addClass('order-2');
+        $('#navbar-brand').addClass('order-3');
+        $('#navbar-toggler').addClass('order-4');
+        // $('#navbarSupportedContent').removeClass('hidden');
+        // $('#navbar-icon').removeClass('hidden');
+        // $('#navbar-brand').removeClass('hidden');
+        // $('#navbar-toggler').removeClass('hidden');
     }
     // For desktop screen size, keep navbar toggle pinned to top and extend menu down
     if($(window).width() > 500) {
+        // $('#navbarSupportedContent').addClass('hidden');
+        // $('#navbar-icon').addClass('hidden');
+        // $('#navbar-brand').addClass('hidden');
+        // $('#navbar-toggler').addClass('hidden');
         $('#navbarSupportedContent').removeClass('order-1');
-        $('#navbar-brand').removeClass('order-2');
-        $('#navbar-toggler').removeClass('order-3');
+        $('#navbar-icon').removeClass('order-2');
+        $('#navbar-brand').removeClass('order-3');
+        $('#navbar-toggler').removeClass('order-4');
+        // $('#navbarSupportedContent').removeClass('hidden');
+        // $('#navbar-icon').removeClass('hidden');
+        // $('#navbar-brand').removeClass('hidden');
+        // $('#navbar-toggler').removeClass('hidden');
     }
 }
 
 $(document).ready(function(){
-    navbarTogglePin()
+    if($(window).width() > 500) {
+        navbarTogglePin()
+    }
 
     // Page-loading spinner animation
     $(window).on('pageshow', function() {
