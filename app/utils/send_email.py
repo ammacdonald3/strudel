d11e2ff7805bef8bbd977ff9cf7be2f6afd33002
Andrew MacDonald <ammacdonald3@gmail.com>
2022-09-19 20:59:46 -0400

    Password reset debugging code

--- app/utils/send_email.py
+++ app/utils/send_email.py
@@ -10,7 +10,12 @@ def send_async_email(app, msg):
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
+    print('---------MSG----------')
+    print(msg)
+    print(msg.body)
+    print(msg.html)
     msg.body = text_body
     msg.html = html_body
+    print('---------MSG----------')
     Thread(target=send_async_email,
            args=(current_app._get_current_object(), msg)).start()
